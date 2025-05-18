1aca1d716bd8fba5dbd7108c4388bc2330b2ddb0
Priyanshi Tyagi <priyanshityagiofficial4567@gmail.com>
2025-05-18 20:41:48 +0530

    aboutpic

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -44,7 +44,7 @@ const About = () => {
             <div className="h-dvh w-screen" id="clip">
                 <div className="mask-clip-path about-image">
                     <img
-                        src="img/mascot.webp"
+                        src="img/about.webp"
                         alt="Background"
                         className="absolute left-0 top-0 size-full object-cover"
                     />
