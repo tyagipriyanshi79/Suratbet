370bfbd14dc9d4ed0b9e7d1610695f4ebab01621
Priyanshi Tyagi <priyanshityagiofficial4567@gmail.com>
2025-05-19 15:49:51 +0530

    image-changes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -23,10 +23,7 @@ const Contact = () => {
         </div>
 
         <div className="absolute -top-40 left-20 w-60 sm:top-1/2 md:left-auto md:right-10 lg:top-20 lg:w-80 max-sm:right-0 max-sm:scale-[60%]">
-          <ImageClipBox
-            src="/img/swordman-partial.webp"
-            clipClass="absolute md:scale-125"
-          />
+          
           <ImageClipBox
             src="/img/swordman.webp"
             clipClass="sword-man-clip-path md:scale-125"
--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -184,7 +184,8 @@ Yoğunluk durumuna göre bu süre değişebilir."
                 <BentoTilt className="bento-tilt_2">
                     <div className="flex size-full flex-col justify-between bg-violet-300 p-5">
                         <h1 className="bento-title special-font max-w-64 text-black">
-                            M<b>o</b>re co<b>m</b>ing s<b>o</b>on.
+                            Kazancın Yeni Adresi!
+SüratBet
                         </h1>
 
                         <TiLocationArrow className="m-5 scale-[5] self-end" />
@@ -192,13 +193,7 @@ Yoğunluk durumuna göre bu süre değişebilir."
                 </BentoTilt>
 
                 <BentoTilt className="bento-tilt_2">
-                    <video
-                        src="videos/feature-5.mp4"
-                        loop
-                        muted
-                        autoPlay
-                        className="size-full object-cover object-center"
-                    />
+                    <img src="/img/suratlogo.png"/>
                 </BentoTilt>
             </div>
         </div>
